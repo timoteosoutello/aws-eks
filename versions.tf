501ffb76efe813830c20303aef8abc45eb1eef79
Timoteo Soutello <timoteo.soutello@8x8.com>
2020-11-03 15:41:25 -0300

    adjust versions

--- versions.tf
+++ versions.tf
@@ -1,6 +1,26 @@
 
 terraform {
-  required_version = ">= 0.12"
+  required_version = ">= 0.13"
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+    kubernetes = {
+      source = "hashicorp/kubernetes"
+    }
+    local = {
+      source = "hashicorp/local"
+    }
+    null = {
+      source = "hashicorp/null"
+    }
+    random = {
+      source = "hashicorp/random"
+    }
+    template = {
+      source = "hashicorp/template"
+    }
+  }
 }
 
 provider "random" {
